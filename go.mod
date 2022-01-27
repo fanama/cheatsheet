543f7481770fc30512e034f5ae87d5f1daf8f5a6
fanama <fana.rakotoasimbola@clirisgroup.com>
2022-01-27 20:59:19 +0100

    cheatsheet start

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/fanama/cheatsheet
+
+go 1.16
+
+require (
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
+	golang.org/x/net v0.0.0-20220127074510-2fabfed7e28f // indirect
+	jaytaylor.com/html2text v0.0.0-20211105163654-bc68cce691ba
+)
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"jaytaylor.com/html2text"
+)
+
+func main() {
+
+	fmt.Println("===========================================================")
+	if len(os.Args) < 2 {
+		fmt.Println("not enough argument")
+		os.Exit(1)
+	}
+
+	field := os.Args[1]
+	topic := ""
+
+	if len(os.Args) > 2 {
+		topic = os.Args[2]
+	}
+
+	fmt.Println("===========================================================")
+	fmt.Println("Getting ", field, "/", topic+"...")
+
+	resp, err := http.Get("http://cheat.sh/" + field + "/" + topic)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	text, err := html2text.FromString(string(body), html2text.Options{PrettyTables: true})
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(text)
+
+}
